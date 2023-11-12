21c10edafb4e28ba9f55f0ee4c2a5df82c11dae5
chinmay <chinmay.mohanty@rixosys.com>
2023-11-12 09:59:11 +0530

    my first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,13 @@
+import hero from './hero.jpg'
+import logo from './logo.svg'
+
+const typof = "https://assets-global.website-files.com/6344fe0ae449f96ce795b92b/635ef94c41528ad6b8ebd9df_Asset%201.svg"
+const magtapp = "https://media.licdn.com/dms/image/C4D0BAQGOVisBbsV0fg/company-logo_200_200/0/1648196408296/magtapp_logo?e=1707955200&v=beta&t=7SAwua1-NeWfFgGqtUkw-VuFJbSzEMFCURbGDd_iGZg"
+const rixosys = "https://media.licdn.com/dms/image/C510BAQG5kVMotA03lg/company-logo_200_200/0/1630614542494/rixosys_logo?e=1707955200&v=beta&t=Mlz_KAdyb3Gh55YiHCoPqx__13Xt2SekRLJyfD9ZGEM"
+export {
+    hero,
+    typof,
+    magtapp,
+    rixosys,
+    logo
+}
